da1e73e2f5f5aefaeba4b1993538183f7446e367
Alex Neigher <alexneigher@Alexs-MacBook-Pro.local>
2018-05-29 19:39:31 -0700

    add pagination for user desc page

--- app/controllers/users/descriptions_controller.rb
+++ app/controllers/users/descriptions_controller.rb
@@ -3,7 +3,7 @@ module Users
 
     def index
       @user = User.find(params[:user_id])
-      @descriptions = @user.descriptions.order(created_at: :desc).includes(:likes, tweak: :title)
+      @descriptions = @user.descriptions.order(created_at: :desc).includes(:likes, tweak: :title).page(params[:page]).per(50)
     end
 
   end
